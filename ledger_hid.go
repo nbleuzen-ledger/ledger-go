d07b6f1f7bcd466ecb8f6a8c9961e6364cbbd315
Nathan Bleuzen <nathan.bleuzen@ledger.fr>
2022-10-18 15:56:27 +0200

    feat: add support for Ledger Nano S+

--- ledger_hid.go
+++ ledger_hid.go
@@ -43,6 +43,7 @@ type LedgerDeviceHID struct {
 
 // list of supported product ids as well as their corresponding interfaces
 var supportedLedgerProductID = map[uint16]int{
+	0x5011: 0, // Ledger Nano S+
 	0x4011: 0, // Ledger Nano X
 	0x1011: 0, // Ledger Nano S
 	0x1:    0, // Ledger Nano S
